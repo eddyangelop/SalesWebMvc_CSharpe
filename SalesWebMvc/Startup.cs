e4c727324dee9b42c02dee4c8dac5bd9b08429b0
eddyangelop <eddysoullive@gmail.com>
2023-04-08 22:40:08 -0300

    migrando string de conexão para sqlServer basico

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -41,7 +41,8 @@ namespace SalesWebMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+            // or UseMySql
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                         builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
